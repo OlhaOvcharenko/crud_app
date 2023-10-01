bb709f6cb1dfc05dc24b27295fbcd1b47e4b9b38
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-10-01 13:25:49 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <main>
+      <Routes>
+          <Route path="/" element={'HOME'}/>
+          <Route path="/post/:id" element={'Single Page'}/>
+          <Route path="/post/add" element={'Add Page'}/>
+          <Route path="/post/edit/:id" element={'Edit Page'} />
+          <Route path="/about" element={'About'}/>
+          <Route path="*" element={'Not found'}/>
+        </Routes>
+    </main>
   );
 }
 
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,3 +1,6 @@
-const initialState = {}
+const initialState = {
+  posts:[],
+  
+}
 
 export default initialState;
