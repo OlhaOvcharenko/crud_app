7463959cee9612a1a2006285726822c9c8842786
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-10-03 21:51:12 +0300

    change element Allposts.js and Home.js

--- src/components/features/AllPosts/AllPosts.js
+++ src/components/features/AllPosts/AllPosts.js
@@ -1,20 +1,23 @@
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
-
+import  { Col } from "react-bootstrap";
 
 const AllPosts = (props) => {
  
     return(
 
-      <Card style={{ width: '20rem' }}>
+      <Col  className="col-lg-4 col-sm-12 col-md-6 px-2 py-2">
+      <Card >
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <Card.Subtitle className="mb-2">Author:{props.author}</Card.Subtitle>
-          <Card.Subtitle className="mb-2">Published: {props.publishedDate}</Card.Subtitle>
+          <Card.Text className="mb-2"><b>Author:</b>  {props.author}</Card.Text>
+          <Card.Text className="mb-2"><b>Published:</b> {props.publishedDate}</Card.Text>
           <Card.Text>{props.shortDescription}</Card.Text>
           <Button variant="primary">Read more</Button>
         </Card.Body>
       </Card>
+      </Col>
+      
     )
 }
 
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,25 +2,23 @@ import AllPosts from "../../features/AllPosts/AllPosts";
 import { getAllPosts } from "../../../redux/postsRedux";
 import { useSelector } from "react-redux";
 import { Row } from "react-bootstrap";
-import  { Col } from "react-bootstrap";
-
+import { Button } from "react-bootstrap";
+import  {Stack } from "react-bootstrap";
 const Home = props => {
 
     const posts = useSelector((state) => getAllPosts(state, props.id))
 
     return(
         <div>
-            <h1>All posts</h1>
-
-            <Row>
-                <Col>
-                    {posts.map((post) => (
-                    <AllPosts key={post.id} {...post} />
-                    ))}
-                </Col>
+           <Stack direction="horizontal" gap={4}>
+                <div className="p-2"><h1>All posts </h1></div>
+                <div className="p-2 ms-auto" ><Button variant="outline-info">Add post</Button>{' '}</div>
+            </Stack>
+            <Row className="row my-5">
+                {posts.map((post) => (
+                <AllPosts key={post.id} {...post} />
+                ))}
             </Row>
-          
-
         </div>
     )
 }
